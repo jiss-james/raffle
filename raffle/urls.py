2b83c24137f99412c97dabd3ec51a9df135bb9b5
Jiss James <jissjames360@gmail.com>
2020-07-05 14:53:56 +0100

    restyling and adding enter comp

--- raffle/urls.py
+++ raffle/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     url(r'^$', views.index, name='index'),
     path('new_comp', views.new_comp, name='new_comp'),
-    path('edit_comp/<str:id>', views.edit_comp, name='edit_comp')
+    path('edit_comp/<str:id>', views.edit_comp, name='edit_comp'),
+    path('enter_comp/<str:id>', views.enter_comp, name='enter_comp')
 
 ]
--- raffle/views.py
+++ raffle/views.py
@@ -49,3 +49,22 @@ def edit_comp(request, id):
             return HttpResponseRedirect('/')
     return render(request, 'edit_comp.html', {'form': form})
 
+
+def enter_comp(request, id):
+
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EntryForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            form.save(commit=True)
+            return HttpResponseRedirect('/')
+
+        # if a GET (or any other method) we'll create a blank form
+    else:
+        form = EntryForm()
+    return render(request, 'enter_comp.html', {'form': form})
+
