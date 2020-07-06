1f070a07782b28c5e39e9c17d23cd0362bc2fcd9
Helana Maliakal <maliakaa@tcd.ie>
2020-07-06 23:13:27 +0100

    everything should be upto date

--- rafflesite/settings.py
+++ rafflesite/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-LOGIN_REDIRECT_URL = "/<user>"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 ROOT_URLCONF = 'rafflesite.urls'
